86d9d99029cd0fc11c011d5f189312d1353d60b8
anthon11786 <44326656+anthon11786@users.noreply.github.com>
2023-03-26 22:34:58 -0400

    Init Commit - Repo setup

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'; //to get native menu of selecting image
+
+// Import Imageviewer from local components dir 
+import ImageViewer from './components/ImageViewer.js';
+import Button from './components/Button.js';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true, 
+      quality: 1,
+    });
+    // Hanlde User result 
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select an image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer 
+          placeholderImageSource={PlaceholderImage}   
+          selectedImage={selectedImage} 
+        />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo!" onPress={pickImageAsync}/>
+        <Button label="Use this photo"/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +46,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageContainer: {
+    flex: 1, 
+    paddingTop: 58,
+  },
+  image: {
+    width: 320, 
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1/3, 
+    alignItems: 'center',
+  }
 });
